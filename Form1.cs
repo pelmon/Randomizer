72a024ff5dcd05875544e4931b2165cc55a1da8a
pelmon <kacuraarsenij@gmail.com>
2020-04-15 13:48:10 +0300

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Randomizer
+{
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+        private int number = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonOfRandom_Click(object sender, EventArgs e)
+        {
+            number = rnd.Next(0, 100);
+            textBox1.Text = number.ToString();
+        }
+    }
+}
